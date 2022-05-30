c70bc4a0799f1a86fa1ececfe59747168293faf1
邵静 <sj01068346@alibaba-inc.com>
2022-05-30 15:30:16 +0800

    test: add module test cases related to keentune target
    Add module test cases related to keentune target
    
    Signed-off-by: 邵静 <sj01068346@alibaba-inc.com>

--- test/main.py
+++ test/main.py
@@ -4,6 +4,7 @@ import unittest
 
 from test_target_configure import TestTargetConfigure
 from test_target_backup import TestTargetBackup
+from test_target_rollback import TestTargetRollback
 
 os.chdir(os.path.abspath(os.path.join(os.getcwd(), "test")))
 
@@ -12,6 +13,7 @@ def RunModelCase():
     suite = unittest.TestSuite()
     suite.addTest(TestTargetConfigure('test_target_server_FUN_configure'))
     suite.addTest(TestTargetBackup('test_target_server_FUN_backup'))
+    suite.addTest(TestTargetRollback('test_target_server_FUN_rollback'))
     return suite
 
 
